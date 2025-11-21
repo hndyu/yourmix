1a2beb5eb08144730dad0556ff29cec5a110c71a
hndyu <4225495+hndyu@users.noreply.github.com>
2025-11-21 09:52:39 +0900

    mix-sectionのユニットテストを作成

--- app/_components/mix-section.tsx
+++ app/_components/mix-section.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import * as React from "react";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import type { Category, Ingredient } from "../types/cocktail";
