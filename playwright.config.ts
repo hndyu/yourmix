baaf4b42981c75bb5cca1e3184fe5c4e6c09e21f
hndyu <4225495+hndyu@users.noreply.github.com>
2025-11-30 22:31:52 +0900

    PlaywrightのGithubAction修正

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
 	/* Retry on CI only */
 	retries: process.env.CI ? 2 : 0,
 	/* Opt out of parallel tests on CI. */
-	workers: process.env.CI ? 12 : undefined,
+	workers: process.env.CI ? 4 : undefined,
 	/* Reporter to use. See https://playwright.dev/docs/test-reporters */
 	reporter: "html",
 	/* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -30,7 +30,7 @@ export default defineConfig({
 		baseURL: "http://localhost:3000",
 
 		/* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-		trace: "on-first-retry",
+		trace: "on",
 	},
 
 	/* Configure projects for major browsers */
