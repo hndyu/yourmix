e769c067b878b48bdb88c4a68dfcd273de2a82ee
hndyu <4225495+hndyu@users.noreply.github.com>
2025-12-02 15:33:28 +0900

    GithubActionのPlaywrightテスト修正

--- playwright.config.ts
+++ playwright.config.ts
@@ -72,10 +72,12 @@ export default defineConfig({
 	],
 
 	/* Run your local dev server before starting the tests */
-	webServer: {
-		command: "npm run dev:test",
-		url: "http://localhost:3000",
-		reuseExistingServer: !process.env.CI,
-		timeout: 300000,
-	},
+	webServer: process.env.CI
+		? undefined
+		: {
+				command: "npm run dev:test",
+				url: "http://localhost:3000",
+				reuseExistingServer: true,
+				timeout: 300000,
+		  },
 });
