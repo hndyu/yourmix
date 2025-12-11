1421bcad9970e53fea765152f84cfddf863d5655
hndyu <4225495+hndyu@users.noreply.github.com>
2025-12-11 14:34:39 +0900

    fix: drizzleのマイグレーション履歴を修正

--- drizzle/0007_acoustic_susan_delgado.sql
+++ drizzle/0007_acoustic_susan_delgado.sql
@@ -6,3 +6,5 @@ CREATE TABLE `delicious_likes` (
 	FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON UPDATE no action ON DELETE cascade,
 	FOREIGN KEY (`cocktail_id`) REFERENCES `cocktails`(`id`) ON UPDATE no action ON DELETE cascade
 );
+--> statement-breakpoint
+CREATE UNIQUE INDEX `delicious_likes_user_id_cocktail_id_unique` ON `delicious_likes` (`user_id`,`cocktail_id`);
