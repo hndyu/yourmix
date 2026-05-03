8eb37eca1fecf9412a00220111f67e356679d276
hndyu <4225495+hndyu@users.noreply.github.com>
2026-05-03 14:39:53 +0900

    chore: fix cd (#116)

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import CocktailMixer from "./_components/cocktail-mixer";
 import { getIngredientsMasterData } from "./lib/ingredients";
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
 	// サーバーサイドで材料マスターデータを取得
 	const { ingredients, categories } = await getIngredientsMasterData();
